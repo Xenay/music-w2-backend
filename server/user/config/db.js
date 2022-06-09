36b96493823ad88d89f7341461f6c897d66a805e
Luka Grubeša <60038862+Xenay@users.noreply.github.com>
2022-06-09 20:34:35 +0200

    Update db.js

--- server/user/config/db.js
+++ server/user/config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
-  database: "mongodb+srv://LukaGrubesa:Kh7njvtu@cluster0.aja8z.mongodb.net",
+  database: "mongodb+srv://LukaGrubesa:*******u@cluster0.aja8z.mongodb.net",
   secret: "password",
 };
